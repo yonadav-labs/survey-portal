2eb6cd0bc14fa9cb581b8d49df6eb2e3e67e1813
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2020-10-20 06:30:09 -0700

    revise admin

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -64,7 +64,7 @@ class EmailUserAdmin(UserAdmin):
     actions_on_bottom = True
     ordering = ('first_name',)
     list_filter = ('is_active',)
-    # autocomplete_fields = ('company',)
+    autocomplete_fields = ('representative',)
     # readonly_fields = ('image_tag',)
     list_display = ('email', 'first_name', 'last_name', 'phone', 'is_superuser')
     search_fields = ('email', 'last_name', 'first_name')
@@ -86,6 +86,7 @@ class EmailUserAdmin(UserAdmin):
         ('Roles', {
             'fields': (
                 'is_superuser',
+                'representative'
             ),
         })
     )
--- apps/representatives/admin.py
+++ apps/representatives/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Representative
+
+class RepresentativeAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'email', 'type', 'status')
+    search_fields = ('first_name', 'last_name', 'email')
+    list_filter = ('type', 'status')
+
+
+admin.site.register(Representative, RepresentativeAdmin)
