0a5400b4f2707dd391688d9f9837dfa657ae97cb
yonadav-labs <yonadav.barilan.upwork@gmail.com>
2020-10-30 09:56:53 -0700

    add audit admin

--- apps/audits/admin.py
+++ apps/audits/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class ChoiceAnswerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'enabled', 'category')
+    search_fields = ('name',)
+    list_filter = ('enabled', 'category')
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'enabled', 'category')
+    list_filter = ('type', 'enabled', 'category')
+    search_fields = ('name',)
+
+
+class TemplateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'enabled', 'category')
+    list_filter = ('enabled', 'category')
+    search_fields = ('name',)
+
+
+class AuditAdmin(admin.ModelAdmin):
+    list_display = ('representative', 'template', 'call_date', 'phone', 'department')
+    list_filter = ('template',)
+    search_fields = ('template__name', 'phone')
+
+
+class ResponseAdmin(admin.ModelAdmin):
+    list_display = ('audit', 'question')
+    search_fields = ('audit__name', 'question__name')
+
+
+class AttachmentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'file_extension', 'audit')
+    list_filter = ('file_extension',)
+    search_fields = ('name', 'audit__name')
+
+
+admin.site.register(ChoiceAnswer, ChoiceAnswerAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Template, TemplateAdmin)
+admin.site.register(Audit, AuditAdmin)
+admin.site.register(Response, ResponseAdmin)
+admin.site.register(Attachment, AttachmentAdmin)
